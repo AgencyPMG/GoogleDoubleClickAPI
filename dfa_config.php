d6487fe21fcde42261d5f9c85281539ea609c674
Chris Alvares <mail@chrisalvares.com>
2012-02-17 11:00:33 -0600

    Update dfa_config.php

--- dfa_config.php
+++ dfa_config.php
@@ -9,8 +9,8 @@ Created by PMG <mailto:seo@pmg.co>, Chris Alvares<mailto:chris.alvares@pmg.co>
 Please note: This is not your Google Account Username and Password, it is the Username and password for DoubleClick
 */
 
-define("DFA_DEFAULT_USERNAME", "chrisalvares");
-define("DFA_DEFAULT_PASSWORD", "apitest123");
+define("DFA_DEFAULT_USERNAME", "ENTER_USERNAME_HERE");
+define("DFA_DEFAULT_PASSWORD", "ENTER_PASSWORD_HERE");
 
 /*
 Note: If you have a token that you already recieved somehow, then place it here, otherwise, keep this blank
@@ -18,12 +18,12 @@ Having a token here may speed things up a little bit, but the token expiration t
 Using the token is good for testing purposes.
 */
 
-define("DFA_DEFAULT_APITOKEN", "7105f75667c6e8a9004cec501a893c76");
+define("DFA_DEFAULT_APITOKEN", "ENTER_EXISTING_TOKEN_HERE");
 //define("DFA_DEFAULT_APITOKEN", "");
 define("DFA_VERSION", "1.16");
 define("DFA_NAMESPACE", "http://www.doubleclick.net/dfa-api/");
 
-define("DFA_DEFAULT_APPLICATION_NAME", "DFAPHPAPI");
+define("DFA_DEFAULT_APPLICATION_NAME", "DFA_PHP_API");
 
 
 ?>
